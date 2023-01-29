5cbcabb67d9612067819b0368867cca05e8da26e
MIRZA ZIYAD AHMED BAIG <beygz17@gmail.com>
2023-01-30 00:11:55 +0500

    Pricing UI with Chakra

---
+++ src/components/ListItem.tsx
@@ -0,0 +1,19 @@
+'use client'
+import React from 'react'
+import {Box, HStack, Text, Icon, StackProps} from '@chakra-ui/react'
+import { CheckIcon } from '../icons/Icons'
+
+const ListItem = (props:StackProps) => {
+    console.log(props)
+    const {children, ...rest} = props
+  return (
+    <Box>
+        <HStack as='li' {...rest} spacing='20px'>
+            <Icon as={CheckIcon} w='2px' h='22px' />
+            <Text>{children}</Text>
+        </HStack>
+    </Box>
+  )
+}
+
+export default ListItem
